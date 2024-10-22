7e03404e3fba260673ee74be979c2bb19c6508fe
Sandeep <saisandeepvemula@gmail.com>
2024-10-22 23:32:54 +0100

    Update(ExplorationScene)
    - Resolved issues

--- Assets/Scripts/CompanionTriggerZone.cs
+++ Assets/Scripts/CompanionTriggerZone.cs
@@ -2,34 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Code that runs when player is near the companion
 public class CompanionTriggerZone : MonoBehaviour
 {
-    public GameObject floatingText; 
+    public GameObject floatingText;
     private bool playerNearby = false;
 
     void Start()
     {
-        floatingText.SetActive(false); 
+        floatingText.SetActive(false);
     }
 
+    // What to do when player is near
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             playerNearby = true;
-            floatingText.SetActive(true); 
+            floatingText.SetActive(true);
         }
     }
 
+    // What to do when player is not near
     void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             playerNearby = false;
-            floatingText.SetActive(false); 
+            floatingText.SetActive(false);
         }
     }
 
+    // Constantly check for these conditions and run CompanionAI code if true
     void Update()
     {
         if (playerNearby && Input.GetKeyDown(KeyCode.E))
